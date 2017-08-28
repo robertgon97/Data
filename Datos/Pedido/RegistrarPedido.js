720109eee7320792a0146c88cf2fe56e5213368c
Robert González <robert_gon97@hotmail.com>
2017-08-28 17:26:47 -0400

    El campo de enviar sirve pero no completo

--- Datos/Pedido/RegistrarPedido.js
+++ Datos/Pedido/RegistrarPedido.js
@@ -13,15 +13,21 @@ function Registrar(req,res) {
     pedido.Nombre = req.body.Nombre;
     pedido.Categoria = req.body.Categoria;
     pedido.Descripcion = req.body.Descripcion;
-    
-    pedido.save((err, pedido) => {
-        if (err) {
-            console.log(`Error 500 del Servidor : ${err}`)
-            return res.status(500).send({Mensaje : `Error 500 al Guardar en la Base de Datos`})
-        }
-    })
-    res.status(200).send(pedido)
-    console.log(`Dato Guardado Exitosamente!`)
+    console.log(req.body.Nombre + req.body.Categoria + req.body.Descripcion)
+    if (pedido.Nombre!=null && pedido.Categoria!=null && pedido.Descripcion!=null) {
+        pedido.save((err, pedido) => {
+            if (err) {
+                console.log(`Error 500 del Servidor : ${err}`)
+                return res.status(500).send({Mensaje : `Error 500 al Guardar en la Base de Datos`})
+            }
+        })
+        res.status(200).send(pedido)
+        console.log(pedido)
+        console.log(`Dato Guardado Exitosamente!`)
+    }else{
+        console.log(`Error 400 del usuario : No inserto todos los datos`)
+        return res.status(400).send({Mensaje : `No Ingresastes Todos los datos`})
+    }
 }
 module.exports ={
     Registrar
